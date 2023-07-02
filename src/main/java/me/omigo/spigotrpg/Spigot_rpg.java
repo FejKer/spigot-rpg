166d038d090cc08a44771e7fdcd36c876174f0b3
Oskar Migowski <o.migowski@protonmail.com>
2023-07-02 12:08:07 +0200

    fixed database connection

--- src/main/java/me/omigo/spigotrpg/Spigot_rpg.java
+++ src/main/java/me/omigo/spigotrpg/Spigot_rpg.java
@@ -3,6 +3,7 @@ package me.omigo.spigotrpg;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.nio.file.FileSystems;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,7 +17,11 @@ public final class Spigot_rpg extends JavaPlugin {
     public void onEnable() {
         System.out.println("Hello world from Spigot-Rpg");
         saveDefaultConfig();
-        connectToDatabase();
+        try {
+            connectToDatabase();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Could not find org.h2.Driver class");
+        }
     }
 
     @Override
@@ -24,15 +29,16 @@ public final class Spigot_rpg extends JavaPlugin {
         disconnectFromDatabase();
     }
 
-    private void connectToDatabase() {
+    private void connectToDatabase() throws ClassNotFoundException {
+        Class.forName("org.h2.Driver");
         FileConfiguration config = getConfig();
-        String url = "jdbc:h2:file:" + getDataFolder().getAbsolutePath() + "/" + config.getString("database-name");
+        String url = "jdbc:h2:file:" + getDataFolder().getAbsolutePath() + FileSystems.getDefault().getSeparator() + config.getString("database-name");
 
         try {
             connection = DriverManager.getConnection(url, "sa", "");
-            // Create table example
             try (Statement stmt = connection.createStatement()) {
                 stmt.execute("CREATE TABLE IF NOT EXISTS players (uuid VARCHAR(36), name VARCHAR(16))");
+                System.out.println("Created table");
             }
         } catch (SQLException e) {
             getLogger().severe("Could not connect to the database: " + e.getMessage());
